1a465f6c7047bdcee95244e3e5b7812eef39eb31
Mirra <152490293+mirra9002@users.noreply.github.com>
2024-12-19 13:55:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
+let ul1 = document.getElementById("ul1");
+let addTaskBt = document.getElementById("add-task-bt");
 let input1 = document.getElementById("input1");
-input1.onkeyup = function(){
-    console.log(input1.value);
-    if(input1.value == "привет, козел")
-    {
-        input1.value = "привет, петя";
+let resetBt = document.getElementById("reset-bt");
+
+function addTask(){
+    let newTask = document.createElement('li');
+    if(input1.value != ''){
+        newTask.textContent = input1.value;
     }
+    else {
+        newTask.textContent = "empty task";
+        newTask.style.fontStyle = "italic";
+    }
+
+    newTask.onclick = function(){
+        newTask.classList.toggle("completed");
+    }
+    
+    ul1.appendChild(newTask);
+    input1.value = null;
+}
+
+
+addTaskBt.onclick = addTask;
+    
+    
+input1.addEventListener('keydown', function(event){
+    if(event.key == 'Enter')
+    {
+        addTask();
+    }
+});
+
+resetBt.onclick = function(){
+    ul1.innerHTML = null;
 }
